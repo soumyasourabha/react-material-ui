c17abd660a48087ceec750177769a88bd9ad22d0
soumyasourabha <53940285+soumyasourabha@users.noreply.github.com>
2023-04-12 14:20:26 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './style.css';
+import { Typography } from '@mui/material';
+export default function App() {
+  return (
+    <div>
+      <Typography></Typography>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-material-ui
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-vf4wxd)
